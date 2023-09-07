b1e700607e5847b39381bdc73f644e5bae3e2fc8
jorge <jorge.andre.silva.fernandes@gmail.com>
2023-09-07 22:02:49 +0100

    Initial configurations.

---
+++ src/main/java/com/pipcpaysimplificado/PipcpaysimplificadoApplication.java
@@ -0,0 +1,13 @@
+package com.pipcpaysimplificado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipcpaysimplificadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipcpaysimplificadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pipcpaysimplificado/PipcpaysimplificadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipcpaysimplificado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipcpaysimplificadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
