b04315cbe2dd86dca2f5feb055bb8a2284b128c5
jorge <jorge.andre.silva.fernandes@gmail.com>
2023-09-07 22:03:57 +0100

    Create repositories.

---
+++ src/main/java/com/pipcpaysimplificado/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.pipcpaysimplificado.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserById(Long id);
+
+
+}
