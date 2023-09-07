c3ce974b79b11c9c1e303319171e6d95b612f68a
jorge <jorge.andre.silva.fernandes@gmail.com>
2023-09-07 22:08:50 +0100

    Update UserRepository.

--- src/main/java/com/pipcpaysimplificado/repositories/UserRepository.java
+++ src/main/java/com/pipcpaysimplificado/repositories/UserRepository.java
@@ -1,6 +1,9 @@
 package com.pipcpaysimplificado.repositories;
 
+import com.pipcpaysimplificado.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByDocument(String document);
