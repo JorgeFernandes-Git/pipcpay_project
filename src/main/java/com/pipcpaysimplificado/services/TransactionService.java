d3884c31edef10f5e11ee59d2ae2fcebb3bde847
jorge <jorge.andre.silva.fernandes@gmail.com>
2023-09-11 19:40:19 +0100

    Add Transactions.

--- src/main/java/com/pipcpaysimplificado/services/TransactionService.java
+++ src/main/java/com/pipcpaysimplificado/services/TransactionService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Service
@@ -31,12 +32,31 @@ public class TransactionService {
 
         userService.validateTransaction(sender, transaction.value());
 
-        if()
+        boolean isAuthorized = this.authorizeTransaction(sender, transaction.value());
+        if(!isAuthorized) {
+            throw new Exception("Transação não autorizada");
+        }
+
+        Transaction newTransaction = new Transaction();
+        newTransaction.setAmount(transaction.value());
+        newTransaction.setSender(sender);
+        newTransaction.setReceiver(receiver);
+        newTransaction.setTimestamp(LocalDateTime.now());
+
+        sender.setBalance(sender.getBalance().subtract(transaction.value()));
+        receiver.setBalance(receiver.getBalance().add(transaction.value()));
+
+       this.repository.save(newTransaction);
+       this.userService.saveUser(sender);
+       this.userService.saveUser(receiver);
     }
 
-    public boolean authorizeTransaction(User sender, BigDecimal value()){
-       ResponseEntity<Map> = restTemplate.getForEntity("https://...", Map.class);
+    public boolean authorizeTransaction(User sender, BigDecimal value) {
+       ResponseEntity<Map> authorizationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6", Map.class);
 
-       if(authorizationRespoonse.getStatusCode() == HttpStatus.OK && authorizationResponse.get)
+       if(authorizationResponse.getStatusCode() == HttpStatus.OK) {
+            String message = (String) authorizationResponse.getBody().get("message");
+            return "Autorizado".equalsIgnoreCase(message);
+        } else return false;
     }
 }
